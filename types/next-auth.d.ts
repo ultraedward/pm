8b8948f7294f4106103d8cd4c60225e3de7845a6
Edward <ultra.edward@gmail.com>
2026-01-08 10:32:16 -0800

    Fix NextAuth isPro typing

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,20 +1,14 @@
-// types/next-auth.d.ts
-import "next-auth";
-import "next-auth/jwt";
+import { DefaultSession } from "next-auth"
 
 declare module "next-auth" {
   interface Session {
-    user?: {
-      id?: string;
-      name?: string | null;
-      email?: string | null;
-      image?: string | null;
-    };
+    user: {
+      id: string
+      isPro: boolean
+    } & DefaultSession["user"]
   }
-}
 
-declare module "next-auth/jwt" {
-  interface JWT {
-    id?: string;
+  interface User {
+    isPro: boolean
   }
 }
