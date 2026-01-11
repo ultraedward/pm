c53a220eff1e785631a526fbb7627e6763cba57e
Edward <ultra.edward@gmail.com>
2026-01-10 23:31:46 -0800

    Fix prices APIs causing 500s

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,16 @@
 import { PrismaClient } from "@prisma/client"
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
 }
 
 export const prisma =
-  globalForPrisma.prisma ??
+  global.prisma ||
   new PrismaClient({
     log: ["error"],
   })
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma
+  global.prisma = prisma
 }
