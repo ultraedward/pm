d7a80919bc26f7e08244f46b5c505e8eb08e4b94
Edward <ultra.edward@gmail.com>
2026-01-13 18:38:33 -0800

    Fix NextAuth callback loop (v4 stable)

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,18 +1,12 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 
 declare module "next-auth" {
   interface Session {
     user: {
-      id: string
-      isPro: boolean
-      name?: string | null
-      email?: string | null
-      image?: string | null
-    }
-  }
-
-  interface User {
-    id: string
-    isPro: boolean
+      id: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
   }
 }
