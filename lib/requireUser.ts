ae7992025b4e4d041765caa76ef87d8596a0cc01
Edward <ultra.edward@gmail.com>
2026-02-09 15:11:11 -0800

    Redirect unauthenticated users to sign-in instead of throwing

--- lib/requireUser.ts
+++ lib/requireUser.ts
@@ -1,11 +1,12 @@
 import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import { authOptions } from "@/lib/authOptions";
 
 export async function requireUser() {
   const session = await getServerSession(authOptions);
 
   if (!session?.user) {
-    throw new Error("Unauthorized");
+    redirect("/api/auth/signin");
   }
 
   return session.user;
