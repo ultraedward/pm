2001092ce8ba52f65967c5e112556482773da694
Edward <ultra.edward@gmail.com>
2026-02-10 15:49:55 -0800

    Fix Stripe API version to match SDK

--- lib/stripe/stripe.ts
+++ lib/stripe/stripe.ts
@@ -1,5 +1,8 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20",
-});
+export const stripe = new Stripe(
+  process.env.STRIPE_SECRET_KEY!,
+  {
+    apiVersion: "2023-10-16",
+  }
+);
