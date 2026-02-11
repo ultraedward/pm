d75ebb970048be173fb5a023fd0fbc45f6cd81c3
Edward <ultra.edward@gmail.com>
2026-02-10 16:47:24 -0800

    Unify Stripe client and fix API version

--- lib/stripe/openBillingPortal.ts
+++ lib/stripe/openBillingPortal.ts
@@ -1,29 +1,17 @@
-import Stripe from "stripe";
-import { prisma } from "@/lib/prisma";
+import { stripe } from "@/lib/stripe/client";
 import { requireUser } from "@/lib/requireUser";
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16",
-});
 
 export async function openBillingPortal() {
   const user = await requireUser();
 
-  const dbUser = await prisma.user.findUnique({
-    where: { id: user.id },
-    select: {
-      stripeCustomerId: true,
-    },
-  });
-
-  if (!dbUser?.stripeCustomerId) {
-    throw new Error("No Stripe customer found for user");
+  if (!user.stripeCustomerId) {
+    throw new Error("No Stripe customer found");
   }
 
-  const portalSession = await stripe.billingPortal.sessions.create({
-    customer: dbUser.stripeCustomerId,
+  const session = await stripe.billingPortal.sessions.create({
+    customer: user.stripeCustomerId,
     return_url: `${process.env.NEXTAUTH_URL}/pricing`,
   });
 
-  return portalSession.url;
+  return session.url;
 }
